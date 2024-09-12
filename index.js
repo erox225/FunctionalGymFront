593add889a5affc490c2d0197319672e55e3e408
Alejandro <noesvenezuela@gmail.com>
2024-09-12 21:57:31 +0200

    Primera subida

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/app.json';
+import { name as appName } from './src/app.json';
+import { render } from 'react-dom';
 
 AppRegistry.registerComponent(appName, () => App);
+
+AppRegistry.runApplication(appName, {
+  initialProps: {},
+  rootTag: document.getElementById('app-root'),
+});
+
+render(<App />, document.getElementById('app-root'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development', // O 'production' si estás en producción
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      'react-native$': 'react-native-web',
+    },
+    extensions: ['.web.js', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),  // Usa "static" en lugar de "contentBase"
+    },
+    compress: true,
+    port: 9000,
+  },
+};
